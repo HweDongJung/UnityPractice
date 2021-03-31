67774802f206f9f21762b60ae3e15fc3821b18cb
HweDongJung <asky1193@gmail.com>
2021-03-31 22:11:33 +0900

    어메이징 볼링 포신, 포탄, 프롭과 데미지 시스템 구현

---
+++ Amazing Bowling/Assets/ShooterRotator.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterRotator : MonoBehaviour
+{
+    private enum RotateState
+    { 
+        Idle,Vertical,Horizontal, Ready // 0,1,2,3 으로 상태 나뉨 
+    }
+    private RotateState state = RotateState.Idle; // RotateState.Idle = 0
+
+    public float verticalRotateSpeed = 360f;
+    public float horizontalRotateSpeed = 360f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case RotateState.Idle:
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    state = RotateState.Horizontal;
+                }
+                break;
+            case RotateState.Horizontal:
+                if (Input.GetButton("Fire1"))
+                {
+                    transform.Rotate(new Vector3(0, horizontalRotateSpeed * Time.deltaTime, 0));
+                }
+                else if (Input.GetButtonUp("Fire1"))
+                {
+                    state = RotateState.Vertical;
+                }
+                break;
+            case RotateState.Vertical:
+                if (Input.GetButton("Fire1"))
+                {
+                    transform.Rotate(new Vector3(-verticalRotateSpeed * Time.deltaTime, 0, 0));
+                }
+                else if (Input.GetButtonUp("Fire1"))
+                {
+                    state = RotateState.Ready;
+                }
+                break;
+            case RotateState.Ready:
+
+                break;
+            default:
+                break;
+
+        }
+        
+
+    }
+}
