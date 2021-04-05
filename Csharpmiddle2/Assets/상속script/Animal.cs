7f25865f5b4d82b48e02cba6f203c7eb35ec3a50
HweDongJung <asky1193@gmail.com>
2021-04-05 20:22:29 +0900

    C# 상속 예제 추가

---
+++ Csharpmiddle2/Assets/상속script/Animal.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal
+{
+    public string name;
+    public float weight;
+    public int year;
+
+    public void print()
+    {
+        Debug.Log(name + " 몸무게: " + weight + " 나이: " + year);
+    }
+
+    protected float GetSpeed()//protected 자식은 접근 가능, 그 외에는 불가능 
+    {
+        return CalcSpeed();
+    }
+
+    private float CalcSpeed() // private는 자식 클래스에 사용 불가능 
+    {
+
+        return 100f / (weight * year);
+    }
+
+}
+
+public class Dog: Animal
+{
+    public void Hunt()
+    {
+        float speed = GetSpeed();
+        Debug.Log(speed + "의 속도로 달려가서 사냥");
+
+        weight += 10f;
+    }
+}
+
+public class Cat : Animal 
+{
+    public void Stealth()
+    {
+        Debug.Log("숨기");
+
+    }
+
+}
---
+++ Csharpmiddle2/Assets/상속script/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cat tom = new Cat();
+        tom.name = "톰";
+        tom.weight = 1.5f;
+        tom.year = 3;
+
+        Dog pike = new Dog();
+        pike.name = "pike";
+        pike.weight = 8f;
+        pike.year = 5;
+
+        tom.print();
+        tom.Stealth();
+        pike.Hunt();
+        pike.print();
+    }
+
+  
+}
