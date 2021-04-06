04d1311d7d8d00887ba25ad4487070c662582060
HweDongJung <asky1193@gmail.com>
2021-04-06 17:15:37 +0900

    다형성 예제 추가

--- Csharpmiddle2/Assets/상속script/Test.cs
+++ Csharpmiddle2/Assets/상속script/Test.cs
@@ -22,6 +22,17 @@ public class Test : MonoBehaviour
         tom.Stealth();
         pike.Hunt();
         pike.print();
+
+        Animal[] someAnimals = new Animal[2];
+        someAnimals[0] = tom;
+        someAnimals[1] = pike;
+        tom.Stealth();
+
+        for (int i = 0; i < someAnimals.Length; i++)
+        {
+            someAnimals[i].print();
+        }
+
     }
 
   
