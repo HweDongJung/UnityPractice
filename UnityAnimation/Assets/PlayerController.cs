0aea0b0051b4699fbba814610c6576a274e7331a
HweDongJung <asky1193@gmail.com>
2021-04-09 19:31:31 +0900

    유니티 애니메이션 예제 추가

---
+++ UnityAnimation/Assets/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator anim;
+
+    public Transform target;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetTrigger("Jump");
+        }
+
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", verticalInput);
+        anim.SetFloat("Horizontal", horizontalInput);
+    }
+
+    private void OnAnimatorIK()
+    {
+        anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1.0f);
+        anim.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1.0f);
+
+        anim.SetIKPosition(AvatarIKGoal.LeftHand, target.position);
+        anim.SetIKRotation(AvatarIKGoal.LeftHand, target.rotation);
+
+        anim.SetLookAtWeight(1.0f);
+        anim.SetLookAtPosition(target.position);
+    }
+}
