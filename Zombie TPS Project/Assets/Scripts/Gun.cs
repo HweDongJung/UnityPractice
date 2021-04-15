0ca64f66bc0afdde55b8e919141d285f874f3180
HweDongJung <asky1193@gmail.com>
2021-04-15 16:37:21 +0900

    Gun 오브젝트 스크립트 구현

--- Zombie TPS Project/Assets/Scripts/Gun.cs
+++ Zombie TPS Project/Assets/Scripts/Gun.cs
@@ -47,17 +47,24 @@ public class Gun : MonoBehaviour
 
     private void Awake()
     {
-
+        gunAudioPlayer = GetComponent<AudioSource>();
+        bulletLineRenderer = GetComponent<LineRenderer>();
+        bulletLineRenderer.positionCount = 2;
+        bulletLineRenderer.enabled = false;
     }
 
     public void Setup(PlayerShooter gunHolder)
     {
-
+        this.gunHolder = gunHolder;
+        excludeTarget = gunHolder.excludeTarget;
     }
 
     private void OnEnable()
     {
-        
+        magAmmo = magCapacity;
+        currentSpread = 0f;
+        lastFireTime = 0f;
+        state = State.Ready;
     }
 
     private void OnDisable()
@@ -67,31 +74,104 @@ public class Gun : MonoBehaviour
 
     public bool Fire(Vector3 aimTarget)
     {
+        if (state == State.Ready && Time.time >= lastFireTime + timeBetFire)
+        {
+            var fireDirection = aimTarget - fireTransform.position;
+
+            var xError = Utility.GedRandomNormalDistribution(0f, currentSpread);
+            var yError = Utility.GedRandomNormalDistribution(0f, currentSpread);
+
+            fireDirection = Quaternion.AngleAxis(yError, Vector3.up) * fireDirection;
+            fireDirection = Quaternion.AngleAxis(xError, Vector3.up) * fireDirection;
+
+            currentSpread += 1f / stability;
+
+            lastFireTime = Time.time;
+            Shot(fireTransform.position, fireDirection);
+        }
+
         return false;
     }
     
     private void Shot(Vector3 startPoint, Vector3 direction)
     {
-        
+        RaycastHit hit;
+        Vector3 hitPosition;
+
+        if (Physics.Raycast(startPoint, direction, out hit, fireDistance, ~excludeTarget))
+        {
+            var target = hit.collider.GetComponent<IDamageable>();
+            if (target != null)
+            {
+                DamageMessage damageMessage;
+                damageMessage.damager = gunHolder.gameObject;
+                damageMessage.amount = damage;
+                damageMessage.hitPoint = hit.point;
+                damageMessage.hitNormal = hit.normal;
+
+                target.ApplyDamage(damageMessage);
+            }
+            hitPosition = hit.point;
+        }
+        else 
+        {
+            hitPosition = startPoint + direction * fireDistance;
+        }
+
+        StartCoroutine(ShotEffect(hitPosition));
+
+        magAmmo--;
+        if (magAmmo <= 0) state = State.Empty;
+
     }
 
     private IEnumerator ShotEffect(Vector3 hitPosition)
     {
-        yield return null;
+        muzzleFlashEffect.Play();
+        shellEjectEffect.Play();
+
+        gunAudioPlayer.PlayOneShot(shotClip); // 사운드 클립 장전 후 바로 발사 
+        bulletLineRenderer.SetPosition(0, transform.position);
+        bulletLineRenderer.SetPosition(1,hitPosition);
+
+
+
+        yield return new WaitForSeconds(0.03f);
+
+        bulletLineRenderer.enabled = false;
     }
     
     public bool Reload()
     {
-        return false;
+        if (state == State.Reloading || ammoRemain <= 0 || magAmmo >= magCapacity)
+        {
+            return false;
+        }
+
+        StartCoroutine(ReloadRoutine());
+
+        return true;
     }
 
     private IEnumerator ReloadRoutine()
     {
-        yield return null;
+        state = State.Reloading;
+        gunAudioPlayer.PlayOneShot(reloadClip);
+
+        yield return new WaitForSeconds(reloadTime);
+
+        var ammoToFill = Mathf.Clamp(magCapacity - magAmmo, 0, ammoRemain);
+
+        magAmmo += ammoToFill;
+        ammoRemain -= ammoToFill;
+
+        state = State.Ready;
     }
 
     private void Update()
     {
-
+        currentSpread = Mathf.Clamp(currentSpread, 0f, maxSpread);
+        currentSpread = Mathf.SmoothDamp(currentSpread, 0f, ref currentSpreadVelocity,
+            1f / restoreFromRecoilSpeed);
     }
 }
